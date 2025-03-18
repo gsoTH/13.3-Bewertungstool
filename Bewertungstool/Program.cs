ad3c70e11f38a50db9cea5bd13b56bd206350b2c
M.Thomas <m.thomas@gso.schule.koeln>
2025-03-18 14:19:14 +0100

    Initiale Version

---
+++ Bewertungstool/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Bewertungstool;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ BewertungstoolTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BewertungstoolTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
