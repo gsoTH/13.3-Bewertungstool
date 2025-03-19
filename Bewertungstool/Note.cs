42b5349cb3ab3cc92eadd0bf25e7b1d4eba3cd0f
M.Thomas <m.thomas@gso.schule.koeln>
2025-03-19 10:37:25 +0100

    Kleine Änerungen

--- Bewertungstool/Note.cs
+++ Bewertungstool/Note.cs
@@ -49,21 +49,21 @@ namespace Bewertungstool
             {
                 switch (Prozent)
                 {
-                    case < 0.2: return "6 ";
+                    case < 0.2: return "6";
                     case < 0.26: return "5-";
-                    case < 0.33: return "5 ";
+                    case < 0.33: return "5";
                     case < 0.39: return "5+";
                     case < 0.44: return "4-";
-                    case < 0.5: return "4 ";
+                    case < 0.5: return "4";
                     case < 0.55: return "4+";
                     case < 0.6: return "3-";
-                    case < 0.65: return "3 ";
+                    case < 0.65: return "3";
                     case < 0.7: return "3+";
                     case < 0.75: return "2-";
-                    case < 0.8: return "2 ";
+                    case < 0.8: return "2";
                     case < 0.85: return "2+";
                     case < 0.9: return "1-";
-                    case < 0.95: return "1 ";
+                    case < 0.95: return "1";
                     default: return "1+";
                 }
             }
