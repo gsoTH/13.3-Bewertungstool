a481c4505b1bbd8900d27dd6c2b9101f1a87cfce
M.Thomas <m.thomas@gso.schule.koeln>
2025-03-21 12:09:05 +0100

    Weiterer Fehler eingebaut

--- Bewertungstool/Note.cs
+++ Bewertungstool/Note.cs
@@ -24,22 +24,22 @@ namespace Bewertungstool
             {
                 switch (Prozent)
                 {
-                    case < 0.2:     return 6;
-                    case < 0.26:    return 5.3;
-                    case < 0.33:    return 5;
-                    case < 0.39:    return 4.7;
-                    case < 0.49:    return 4.3;
-                    case < 0.5:     return 4;
-                    case < 0.55:    return 3.7;
-                    case < 0.6:     return 3.3;
-                    case < 0.65:    return 3;
-                    case < 0.7:     return 2.7;
-                    case < 0.75:    return 2.3;
-                    case < 0.8:     return 2;
-                    case < 0.85:    return 1.7;
-                    case < 0.9:     return 1.3;
-                    case < 0.95:    return 1;
-                    default:        return 0.7; 
+                    case <  0.22:    return 6;
+                    case <= 0.26:    return 5.3;
+                    case <= 0.33:    return 5;
+                    case <= 0.39:    return 4.7;
+                    case <= 0.49:    return 4.3;
+                    case <= 0.5:     return 4;
+                    case <= 0.55:    return 3.7;
+                    case <= 0.6:     return 3.3;
+                    case <= 0.65:    return 3;
+                    case <= 0.7:     return 2.7;
+                    case <= 0.75:    return 2.3;
+                    case <= 0.8:     return 2;
+                    case <= 0.85:    return 1.7;
+                    case <= 0.9:     return 1.3;
+                    case <= 0.95:    return 1;
+                    default:         return 0.7; 
                 }
             }
         }
@@ -50,22 +50,22 @@ namespace Bewertungstool
             {
                 switch (Prozent)
                 {
-                    case < 0.2: return "6";
-                    case < 0.26: return "5-";
-                    case < 0.33: return "5";
-                    case < 0.39: return "5+";
-                    case < 0.44: return "4-";
-                    case < 0.5: return "4";
-                    case < 0.55: return "4+";
-                    case < 0.6: return "3-";
-                    case < 0.65: return "3";
-                    case < 0.7: return "3+";
-                    case < 0.75: return "2-";
-                    case < 0.8: return "2";
-                    case < 0.85: return "2+";
-                    case < 0.9: return "1-";
-                    case < 0.95: return "1";
-                    default: return "1+";
+                    case <  0.22: return "6";
+                    case <= 0.26: return "5-";
+                    case <= 0.33: return "5";
+                    case <= 0.39: return "5+";
+                    case <= 0.44: return "4-";
+                    case <= 0.5:  return "4";
+                    case <= 0.55: return "4+";
+                    case <= 0.6:  return "3-";
+                    case <= 0.65: return "3";
+                    case <= 0.7:  return "3+";
+                    case <= 0.75: return "2-";
+                    case <= 0.8:  return "2";
+                    case <= 0.85: return "2+";
+                    case <= 0.9:  return "1-";
+                    case <= 0.95: return "1";
+                    default:      return "1+";
                 }
             }
         }
@@ -76,22 +76,22 @@ namespace Bewertungstool
             {
                 switch (Prozent)
                 {
-                    case < 0.2:  return "ungenügend";
-                    case < 0.26: return "mangelhaft";
-                    case < 0.33: return "mangelhaft";
-                    case < 0.39: return "mangelhaft";
-                    case < 0.44: return "ausreichend";
-                    case < 0.5:  return "ausreichend";
-                    case < 0.55: return "befriedigend";
-                    case < 0.6:  return "befriedigend";
-                    case < 0.65: return "befriedigend";
-                    case < 0.7:  return "befriedigend";
-                    case < 0.75: return "gut";
-                    case < 0.8:  return "gut";
-                    case < 0.85: return "gut";
-                    case < 0.9:  return "sehr gut";
-                    case < 0.95: return "sehr gut";
-                    default:     return "sehr gut";
+                    case <  0.22: return "ungenügend";
+                    case <= 0.26: return "mangelhaft";
+                    case <= 0.33: return "mangelhaft";
+                    case <= 0.39: return "mangelhaft";
+                    case <= 0.44: return "ausreichend";
+                    case <= 0.5:  return "ausreichend";
+                    case <= 0.55: return "befriedigend";
+                    case <= 0.6:  return "befriedigend";
+                    case <= 0.65: return "befriedigend";
+                    case <= 0.7:  return "befriedigend";
+                    case <= 0.75: return "gut";
+                    case <= 0.8:  return "gut";
+                    case <= 0.85: return "gut";
+                    case <= 0.9:  return "sehr gut";
+                    case <= 0.95: return "sehr gut";
+                    default:      return "sehr gut";
                 }
             }
         }
